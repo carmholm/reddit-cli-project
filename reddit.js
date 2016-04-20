604b68bf1366e70a2b88f22a035ef29b7d9203c2
carmholm <ameliacarmenholmes@gmail.com>
2016-04-20 20:07:56 +0000

    NodeJS command-line Reddit clone

--- reddit.js
+++ reddit.js
@@ -1,11 +1,23 @@
 var request = require('request');
+var util = require("util");
+
+//console.log(util.inspect(x, { showHidden: true, depth: null, colors: true })
+
 
 /*
 This function should "return" the default homepage posts as an array of objects
 */
 function getHomepage(callback) {
-  // Load reddit.com/.json and call back with the array of posts
+  var address = "http://www.reddit.com/.json";
+  request(address, function(err, result) {
+    callback(JSON.parse(result.body));
+    // Load reddit.com/.json and call back with the array of posts
+  });
 }
+
+// getHomepage(function(x){
+// console.log(util.inspect(x.data.children, { showHidden: true, depth: 1, colors: true }));
+// });
 
 /*
 This function should "return" the default homepage posts as an array of objects.
@@ -14,7 +26,21 @@ In contrast to the `getHomepage` function, this one accepts a `sortingMethod` pa
 function getSortedHomepage(sortingMethod, callback) {
   // Load reddit.com/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
+  var address = "http://www.reddit.com/";
+  var redditSortingMethods = ["hot", "new", "rising", "controversial", "top", "gilded", "wiki", "promoted"];
+
+  if (redditSortingMethods.indexOf(sortingMethod) !== -1) {
+    var addressSort = address + sortingMethod + "/.json";
+    request(addressSort, function(err, result) {
+      callback(JSON.parse(result.body));
+    });
+  }
 }
+
+getSortedHomepage("old", function(x){
+console.log(util.inspect(x.data.children, { showHidden: true, depth: 1, colors: true }));
+});
+
 
 /*
 This function should "return" the posts on the front page of a subreddit as an array of objects.
