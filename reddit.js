4ad8459bbbd8f4889e85ce4b2e8a066ab2c8d943
carmholm <ameliacarmenholmes@gmail.com>
2016-04-22 19:16:05 +0000

    Sorted result menu added

--- reddit.js
+++ reddit.js
@@ -2,6 +2,7 @@ const imageToAscii = require("image-to-ascii");
 var prompt = require('prompt');
 var request = require('request');
 var colors = require('colors');
+var wrap = require('word-wrap');
 var util = require("util");
 prompt.start();
 
@@ -17,6 +18,35 @@ var menuChoices = [{
   name: 'List subreddits',
   value: 'SUBREDDITS'
 }];
+
+var sortingMethods = [{
+  name: 'Hot',
+  value: 'HOT'
+}, {
+  name: 'New',
+  value: 'NEW'
+}, {
+  name: 'Rising',
+  value: 'RISING'
+}, {
+  name: 'Controversial',
+  value: 'CONTROVERSIAL'
+}, {
+  name: 'Top',
+  value: 'TOP'
+}, {
+  name: 'Gilded',
+  value: 'GILDED'
+}, {
+  name: 'Wiki',
+  value: 'WIKI'
+}, {
+  name: 'Promoted',
+  value: 'PROMOTED'
+}];
+
+
+
 
 function myApp() {
   inquirer.prompt({
@@ -34,6 +64,7 @@ function myApp() {
             value: x.data
           });
         });
+        homepageObj.unshift(new inquirer.Separator(), 'SORT POSTS', new inquirer.Separator());
         inquirer.prompt({
           type: 'list',
           name: 'menu',
@@ -41,18 +72,66 @@ function myApp() {
           choices: homepageObj
         }).then(function(answer) {
           var reddit = answer.menu;
-          console.log("Title".underline.bold + ": " + reddit.title);
-          console.log("Url".underline.bold + ": " + reddit.url);
-          console.log("Author".underline.bold + ": " + reddit.author);
-          console.log("Views".underline.bold + ": " + reddit.score);
-            if(reddit.thumbnail.indexOf('png','img','gif')){
-            imageToAscii(reddit.thumbnail, (err, converted) => {
-            console.log(err || converted);
-            getComments(reddit, function(commentsArray){
-            console.log(commentsArray);
-            myApp();
+          if (answer.menu === "SORT POSTS") {
+            inquirer.prompt({
+              type: 'list',
+              name: 'menu',
+              message: 'Please choose a sorting method:',
+              choices: sortingMethods
+            }).then(function(answer) {
+              var sortingMethod = answer.menu;
+              var sortedObj = [];
+              getSortedHomepage(sortingMethod, function(resultArray) {
+                resultArray.forEach(function(x, i) {
+                  sortedObj.push({
+                    name: x.data.title,
+                    value: x.data
+                  });
+                });
+                sortedObj.unshift(new inquirer.Separator(), 'HOMEPAGE', new inquirer.Separator());
+                inquirer.prompt({
+                  type: 'list',
+                  name: 'menu',
+                  message: "What would you like to see?",
+                  choices: sortedObj
+                }).then(function(answer) {
+                  var sortedItems = answer.menu;
+                  if (answer.menu === "HOMEPAGE") {
+                    myApp();
+                  }
+                  else {
+                    console.log("Title".underline.bold + ": " + sortedItems.title);
+                    console.log("Url".underline.bold + ": " + sortedItems.url.blue);
+                    console.log("Author".underline.bold + ": " + sortedItems.author);
+                    console.log("Views".underline.bold + ": " + sortedItems.score);
+                    if (sortedItems.thumbnail.indexOf('png', 'img', 'gif')) {
+                      imageToAscii(sortedItems.thumbnail, (err, converted) => {
+                        console.log(err || converted);
+                        getComments(sortedItems, function(commentsArray) {
+                          console.log(commentsArray);
+                          myApp();
+                        });
+                      });
+                    }
+                  }
+                });
+              });
             });
-            });
+          }
+          else {
+            console.log("Title".underline.bold + ": " + reddit.title);
+            console.log("Url".underline.bold + ": " + reddit.url.blue);
+            console.log("Author".underline.bold + ": " + reddit.author);
+            console.log("Views".underline.bold + ": " + reddit.score);
+            if (reddit.thumbnail.indexOf('png', 'img', 'gif')) {
+              imageToAscii(reddit.thumbnail, (err, converted) => {
+                console.log(err || converted);
+                getComments(reddit, function(commentsArray) {
+                  console.log(commentsArray);
+                  myApp();
+                });
+              });
+            }
           }
         });
       });
@@ -76,16 +155,16 @@ function myApp() {
           }).then(function(answer) {
             var subredditItem = answer.menu;
             console.log("Title".underline.bold + ": " + subredditItem.title);
-            console.log("Url".underline.bold + ": " + subredditItem.url);
+            console.log("Url".underline.bold + ": " + subredditItem.url.blue);
             console.log("Author".underline.bold + ": " + subredditItem.author);
             console.log("Views".underline.bold + ": " + subredditItem.score);
-              if(subredditItem.thumbnail.indexOf('png','img','gif')){
+            if (subredditItem.thumbnail.indexOf('png', 'img', 'gif')) {
               imageToAscii(subredditItem.thumbnail, (err, converted) => {
-              console.log(err || converted);
-              getComments(subredditItem, function(commentsArray){
-              console.log(commentsArray);
-              myApp();
-              });
+                console.log(err || converted);
+                getComments(subredditItem, function(commentsArray) {
+                  console.log(commentsArray);
+                  myApp();
+                });
               });
             }
           });
@@ -122,27 +201,27 @@ function myApp() {
                 });
               });
 
-        inquirer.prompt({
-              type: 'list',
-              name: 'menu',
-              message: 'What would you like to see?',
-              choices: newSubredditObj
-            }).then(function(answer) {
-              var subredditItem = answer.menu;
-              console.log("Title".underline.bold + ": " + subredditItem.title);
-              console.log("Url".underline.bold + ": " + subredditItem.url);
-              console.log("Author".underline.bold + ": " + subredditItem.author);
-              console.log("Views".underline.bold + ": " + subredditItem.score);
-                if(subredditItem.thumbnail.indexOf('png','img','gif')){
-                imageToAscii(subredditItem.thumbnail, (err, converted) => {
-                console.log(err || converted);
-                getComments(subredditItem, function(commentsArray){
-                console.log(commentsArray);
-                myApp();
-                });
-                });
-              }
-            });
+              inquirer.prompt({
+                type: 'list',
+                name: 'menu',
+                message: 'What would you like to see?',
+                choices: newSubredditObj
+              }).then(function(answer) {
+                var subredditItem = answer.menu;
+                console.log("Title".underline.bold + ": " + subredditItem.title);
+                console.log("Url".underline.bold + ": " + subredditItem.url.blue);
+                console.log("Author".underline.bold + ": " + subredditItem.author);
+                console.log("Views".underline.bold + ": " + subredditItem.score);
+                if (subredditItem.thumbnail.indexOf('png', 'img', 'gif')) {
+                  imageToAscii(subredditItem.thumbnail, (err, converted) => {
+                    console.log(err || converted);
+                    getComments(subredditItem, function(commentsArray) {
+                      console.log(commentsArray);
+                      myApp();
+                    });
+                  });
+                }
+              });
             });
           }
         });
@@ -154,13 +233,13 @@ function myApp() {
 
 myApp();
 
-function getComments(redditArr, callback){
+function getComments(redditArr, callback) {
   var redditCommentURL = "https://www.reddit.com" + redditArr.permalink + ".json";
-  request(redditCommentURL, function(err, result){
+  request(redditCommentURL, function(err, result) {
     var resultArray = JSON.parse(result.body);
     var commentObj = resultArray[1].data.children;
     var commentsArr = [];
-    commentObj.forEach(function(x ,i){
+    commentObj.forEach(function(x, i) {
       commentsArr.push({
         comment: x.data.body
       });
@@ -190,21 +269,32 @@ This function should "return" the default homepage posts as an array of objects.
 In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.
 */
 
-function getSortedHomepage(sortingMethod, callback) {
-  // Load reddit.com/{sortingMethod}.json and call back with the array of posts
-  // Check if the sorting method is valid based on the various Reddit sorting methods
-  var address = "http://www.reddit.com/";
-  var redditSortingMethods = ["hot", "new", "rising", "controversial", "top", "gilded", "wiki", "promoted"];
+// Load reddit.com/{sortingMethod}.json and call back with the array of posts
+// Check if the sorting method is valid based on the various Reddit sorting methods
 
-  if (redditSortingMethods.indexOf(sortingMethod) !== -1) {
-    var addressSort = address + sortingMethod + ".json";
-    request(addressSort, function(err, result) {
-      var resultObject = JSON.parse(result.body);
-      var resultArray = resultObject.data.children
-      callback(resultArray);
-    });
-  }
+function getSortedHomepage(sortingMethod, callback) {
+  var redditSort = "http://www.reddit.com/" + sortingMethod.toLowerCase() + ".json";
+  request(redditSort, function(err, result) {
+    var resultObject = JSON.parse(result.body);
+    var resultArray = resultObject.data.children;
+    callback(resultArray);
+  });
 }
+
+
+
+// function getSortedHomepage(sortingMethod, callback) {
+//   var address = "http://www.reddit.com/";
+//   var redditSortingMethods = ["hot", "new", "rising", "controversial", "top", "gilded", "wiki", "promoted"];
+//   if (redditSortingMethods.indexOf(sortingMethod) !== -1) {
+//     var addressSort = address + sortingMethod + ".json";
+//     request(addressSort, function(err, result) {
+//       var resultObject = JSON.parse(result.body);
+//       var resultArray = resultObject.data.children
+//       callback(resultArray);
+//     });
+//   }
+// }
 
 // getSortedHomepage("new", function(x){
 // console.log(util.inspect(x, { showHidden: true, depth: 1, colors: true }));
